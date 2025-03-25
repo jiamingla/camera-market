7e73491ebecf4873745066ddf422a5012e798c97
half bucket <34878559+jiamingla@users.noreply.github.com>
2025-03-26 00:48:10 +0800

    新增測試用例以驗證創建列表時標籤數量超過上限的情況

--- camera.market/src/test/java/jiamingla/first/camera/market/controller/ListingCreateTest.java
+++ camera.market/src/test/java/jiamingla/first/camera/market/controller/ListingCreateTest.java
@@ -100,4 +100,79 @@ public class ListingCreateTest extends BaseControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+    @Test
+    public void testCreateListingWithTooMuchTags() throws Exception {
+    
+        Listing listing = new Listing();
+        listing.setTitle("test");
+        listing.setDescription("test");
+        listing.setMake(Make.CANON); //使用Make enum
+        listing.setModel("test");
+        listing.setPrice(12);
+        listing.setCategory(Category.DSLR);
+        listing.setType(ListingType.SALE);
+
+        //新增圖片網址
+        List<ListingImage> images = new ArrayList<>();
+        ListingImage listingImage1 = new ListingImage();
+        listingImage1.setImageUrl("https://test1.com");
+        listingImage1.setListing(listing); //重要
+        ListingImage listingImage2 = new ListingImage();
+        listingImage2.setImageUrl("https://test2.com");
+        listingImage2.setListing(listing);//重要
+        images.add(listingImage1);
+        images.add(listingImage2);
+        listing.setImages(images);
+
+        Tag tag1 = new Tag();
+        tag1.setName("二手");
+        tag1.setType(TagType.CONDITION);
+        listing.getTags().add(tag1);
+        Tag tag2 = new Tag();
+        tag2.setName("新北市");
+        tag2.setType(TagType.LOCATION);
+        listing.getTags().add(tag2);
+        Tag tag3 = new Tag();
+        tag3.setName("黑色");
+        tag3.setType(TagType.COLOR);
+        listing.getTags().add(tag3);
+        Tag tag4 = new Tag();
+        tag4.setName("大");
+        tag4.setType(TagType.SIZE);
+        listing.getTags().add(tag4);
+        Tag tag5 = new Tag();
+        tag5.setName("其他");
+        tag5.setType(TagType.OTHER);
+        listing.getTags().add(tag5);
+        Tag tag6 = new Tag();
+        tag6.setName("SONY");
+        tag6.setType(TagType.MAKE);
+        listing.getTags().add(tag6);
+        Tag tag7 = new Tag();
+        tag7.setName("A6000");
+        tag7.setType(TagType.MODEL);
+        listing.getTags().add(tag7);
+        Tag tag8 = new Tag();
+        tag8.setName("CAMERA");
+        tag8.setType(TagType.CATEGORY);
+        listing.getTags().add(tag8);
+        Tag tag9 = new Tag();
+        tag9.setName("台北市");
+        tag9.setType(TagType.LOCATION);
+        listing.getTags().add(tag9);
+        Tag tag10 = new Tag();
+        tag10.setName("北市");
+        tag10.setType(TagType.LOCATION);
+        listing.getTags().add(tag10);
+        Tag tag11 = new Tag();
+        tag11.setName("台北");
+        tag11.setType(TagType.LOCATION);
+        listing.getTags().add(tag11);
+
+        mockMvc.perform(post(API_LISTINGS)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(listing)))
+                .andExpect(status().isBadRequest());
+    }
 }
