3ff98091c40b73392187d92fd77fd7365508f870
half bucket <34878559+jiamingla@users.noreply.github.com>
2025-03-25 16:01:13 +0800

    新增 ListingController 的異常處理器，更新 Listing 和 Tag 實體以增加驗證註解，Tag上限10個，並新增 TagType 列舉類別

--- camera.market/src/main/java/jiamingla/first/camera/market/controller/ListingController.java
+++ camera.market/src/main/java/jiamingla/first/camera/market/controller/ListingController.java
@@ -9,9 +9,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/listings")
@@ -78,4 +82,16 @@ public class ListingController {
         return new ResponseEntity<>(listings, HttpStatus.OK);
     }
 
+    // 新增異常處理器
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 }
